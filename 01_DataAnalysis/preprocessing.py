66a2f59442e0e84efbc0b21c08543ae3752aa4d6
CheonYeRi <hecloz97@naver.com>
2025-12-22 11:17:55 +0900

    docs, feat: 강원도 월별 기온 엑셀 파일 수정, 월별기온 전처리 파일 추가

--- 01_DataAnalysis/preprocessing.py
+++ 01_DataAnalysis/preprocessing.py
@@ -8,11 +8,22 @@ def clean_structure(df):
     df = df.iloc[:-3, :-3]      # 뒤쪽 행/열 제거만
     return df.reset_index(drop=True)
 
+
 #결측값 처리 (평균 값으로 대체)
 def handle_rainfall_missing(df,col):
     if not pd.api.types.is_numeric_dtype(df[col]):
         df[col] = pd.to_numeric(df[col], errors="coerce")
     
+    pre = df[col].mean().round(1)
+    df[col] = df[col].fillna(pre)
+
+    return df
+
+
+def handle_missing(df, col):
+    if not pd.api.types.is_numeric_dtype(df[col]):
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+
     pre = df[col].mean().round(1)
     df[col] = df[col].fillna(pre)
 
